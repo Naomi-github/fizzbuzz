bd8f75647e042407e0b5281157cfdeed1e9ea2bb
Naomi Starr <naomi.starr.mail@gmail.com>
2022-11-03 12:10:17 +0000

    update tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,22 @@ describe 'fizzbuzz' do
   it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'return 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+  it 'return "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'return "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
 end
